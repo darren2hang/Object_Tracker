c127e46c8f5bddf17eb7698212badc35d58b6893
Darren Zhang <56695604+darren2hang@users.noreply.github.com>
2020-08-07 00:58:23 -0700

    Removed unnecessary method

--- src/Point.java
+++ src/Point.java
@@ -27,18 +27,4 @@ public class Point {
         return (other.getY() - getY()) * (other.getY() - getY()) + (other.getX() - getX()) * (other.getX() - getX());
     }
 
-    public void takeRandomStep() {
-        int stepSize = 3;
-        int rand = (int) (Math.random() * 4);
-        if (rand == 0) {
-            setX(getX() + stepSize);
-        } else if (rand == 1) {
-            setY(getY() + stepSize);
-        } else if (rand == 2) {
-            setX(getX() - stepSize);
-        } else if (rand == 3) {
-            setY(getY() - stepSize);
-        }
-    }
-
 }
